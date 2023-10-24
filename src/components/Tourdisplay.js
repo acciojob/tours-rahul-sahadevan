485f378eaa90695c7f9074486154809594012bfd
rahul-sahadevan <134208224+rahul-sahadevan@users.noreply.github.com>
2023-10-24 07:21:15 +0000

    xx

--- src/components/Tourdisplay.js
+++ src/components/Tourdisplay.js
@@ -11,7 +11,7 @@ function Display(){
         console.log(showmore)
         if(showmore === false){
             setShowmore(true);
-            setCard(card.slice(0,3));
+            setCard(card.slice(0,2));
         }
         else{
             setShowmore(false);
